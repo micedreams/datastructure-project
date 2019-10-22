e42d287c32397c4fde15c4c354c44382411ba963
Akshatha <micedreams@gmail.com>
2019-10-23 04:01:21 +0530

    Update inventry_candystore.rb

--- inventry_candystore.rb
+++ inventry_candystore.rb
@@ -1,44 +1,26 @@
-def inventry(number)
-inventry_hash = {}
-(1..number).each do
-  input = ""
-  name = ""
-  count = ""
+def inventory(number)
+  inventory_hash = {}
+  (1..number).each do
+    puts "Type quit to quit, press enter to continue"
+    input = gets.chomp
 
-  puts "Type quit to quit, press enter to continue"
+    break if input == "quit"
 
-  input = gets.chomp
-  break if input == "quit"
+    puts "Enter name of candy: "
+    name = gets.chomp
 
-  puts "Enter name of candy: "
-  name = gets.chomp
-  puts "Enter candy count: "
-  count = gets.chomp
+    puts "Enter candy count: "
+    count = gets.chomp.to_i
 
+    inventory_hash[name] = count
+  end
 
-  inventry_hash[name] = count
+  puts "Here is the content that was in the hash: "
+
+  inventory_hash.each do |key, value|
+    puts " #{key} => #{value}"
+  end
+
+  inventory_hash
 end
 
-puts "Here is the content that was in the hash: "
-
-inventry_hash.each do |key, value|
-  puts " #{key} => #{value}"
-
-
-end
- 
-a=inventry_hash.values
-puts"#{a}"
-
-values_in_int = a.map { |a| a.to_i } # this line converts the string values to int
-sum=values_in_int.sum
-puts "the sum is #{sum}"
-end
-
-puts"**********************************\n\tWelcome to candy shop\n**********************************"
-puts"\nHow many candy flavours count u Want to add :"
-  flavour=gets
-  flavour=flavour.to_i
-  puts"\n"
-
-inventry(flavour)
