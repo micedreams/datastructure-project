2babb9df4d725ac5c8e0ad853cbd3cb8ae024a5a
Akshatha <micedreams@gmail.com>
2019-10-22 16:54:27 +0530

    Update candystore.rb

--- candystore.rb
+++ candystore.rb
@@ -4,7 +4,7 @@ puts"\nHow many candy flavours count u Want to add :"
 flavour=gets
 flavour=flavour.to_i
 puts"\n"
-candystore_hash = {}
+inventry_hash = {}
 (1..flavour).each do
   input = ""
   name = ""
@@ -21,19 +21,21 @@ candystore_hash = {}
   count = gets.chomp
 
 
-  candystore_hash[name] = count
+  inventry_hash[name] = count
 end
 
 puts "Here is the content that was in the hash: "
 
-candystore_hash.each do |key, value|
+inventry_hash.each do |key, value|
   puts " #{key} => #{value}"
 
 
 end
  
-a=candystore_hash.values
+a=inventry_hash.values
 puts"#{a}"
 
 values_in_int = a.map { |a| a.to_i } # this line converts the string values to int
-puts "the sum is #{values_in_int.sum}"
+sum=values_in_int.sum
+puts "the sum is #{sum}"
+
