bb19d17a6607e3dcee97ec9879dd8bce2a52fcbe
Akshatha <micedreams@gmail.com>
2019-10-23 07:35:23 +0530

    Update candystore.rb

--- candystore.rb
+++ candystore.rb
@@ -1,77 +1,65 @@
-def inventry(number)
-  inventry_hash = {}
+def inventory(number)
+  inventory_hash = {}
   (1..number).each do
-    input = ""
-    name = ""
-    count = ""
-  
     puts "Type quit to quit, press enter to continue"
-  
     input = gets.chomp
+
     break if input == "quit"
-  
+
     puts "Enter name of candy: "
     name = gets.chomp
+
     puts "Enter candy count: "
-    count = gets.chomp
-  
-  
-    inventry_hash[name] = count
+    count = gets.chomp.to_i
+
+    inventory_hash[name] = count
   end
-  
-  puts "Here is the content that was in the hash: "
-  
-  inventry_hash.each do |key, value|
-    puts " #{key} => #{value}"
-  
-  
-  end
-   
-  a=inventry_hash.values
-  puts"#{a}"
-  
-  values_in_int = a.map { |a| a.to_i } # this line converts the string values to int
-  sum=values_in_int.sum
-  puts "the sum is #{sum}"
-  end
-  
-  puts"**********************************\n\tWelcome to candy shop\n**********************************"
-  puts"\nHow many candy flavours count u Want to add :"
-    flavour=gets
-    flavour=flavour.to_i
-    puts"\n"
-  
-  inventry(flavour)
 
-
-
-
-puts"----------------------------------------"
-puts"which candy flavour do u want?"
-bought_flavour=gets
-puts"in what quantity do u want #{bought_flavour}candy"
-bought_count=gets
-bought_count=bought_count.to_i
-    inventry_hash = {}
-
-if(inventry_hash.fetch(bought_flavour)>bought_count)
-  inventry_hash[bought_flavour]=inventry_hash.fetch(bought_flavour).to_i - bought_count
-  
-  
-  puts"----------------------------------------"
-  
   puts "Here is the content that was in the hash: "
 
-  inventry_hash.each do |key, value|
+  inventory_hash.each do |key, value|
     puts " #{key} => #{value}"
-  
-  
   end
-   
-  a=inventry_hash.values
-  puts"#{a}"
-  
-  values_in_int = a.map { |a| a.to_i } # this line converts the string values to int
-  sum=values_in_int.sum
-  puts "the sum is #{sum}"
+
+  inventory_hash
 end
+
+
+puts "\nHow many candy flavours count u Want to add :"
+
+flavour_count = gets.chomp.to_i
+
+
+inventory_hash = inventory(flavour_count)
+
+
+
+  sum = inventory_hash.values.sum
+
+  puts "**********************************\n\tWelcome to candy shop\n**********************************"
+  while sum>0
+puts "Here are our flavours "
+
+  inventory_hash.each do |key, value|
+    puts " #{key}"
+  end
+puts "which candy flavour do u want?"
+
+bought_flavour = gets.chomp
+while inventory_hash.fetch(bought_flavour)>0
+puts "in what quantity do u want #{bought_flavour} candy"
+bought_count = gets.chomp
+bought_count = bought_count.to_i
+
+if (inventory_hash.fetch(bought_flavour) > bought_count)
+  inventory_hash[bought_flavour] =
+    inventory_hash.fetch(bought_flavour).to_i - bought_count
+    puts"thank u for shopping"
+    break
+else puts"we only have #{inventory_hash.fetch(bought_flavour)} in stock "
+end
+end
+ puts"we are out of stock"
+end
+puts"we are closed"
+break
